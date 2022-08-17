5b36bdd7eca93d1f592725738009979da2d9caaf
jh665 <jin123@cuk.edu>
2022-08-17 12:45:08 +0900

    대문 페이지 수정

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from .models import Post
+from blog.models import Post
 
 class SingleAbout(ListView):
     model = Post
@@ -7,7 +7,13 @@ class SingleAbout(ListView):
     template_name = 'single_pages/about_me.html'
 
 class SingleHome(ListView):
+    recent_posts = Post.objects.order_by('-pk')[:3]
     model = Post
     template_name = 'single_pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SingleHome, self).get_context_data()
+        context['recent_posts'] = SingleHome.recent_posts
+        return context
 
 
