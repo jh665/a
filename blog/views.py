e2ec594b5096cdc4364e4a08bf4642dda18abb8f
jh665 <jin123@cuk.edu>
2022-08-17 10:45:38 +0900

    pagination으로 포스트를 나누어 보여 주기

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,7 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
     template_name = 'blog/index.html'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
